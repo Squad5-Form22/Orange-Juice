082f4d154a8b2a049a962af22a92363acf6b280b
thazsobral <thazsobral@gmail.com>
2022-11-02 20:08:04 -0300

    adding status in the users

--- app/database/migrations/2014_10_12_000000_create_users_table.php
+++ app/database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->boolean('status')->deafult(true);
             $table->string('role')->default('common');
             $table->rememberToken();
             $table->timestamps();
