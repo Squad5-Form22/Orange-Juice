75e453cb336793b01399f213634ded0ebabfc513
Kriss-Albius <kriss_albius@hotmail.com>
2022-11-04 21:47:34 -0300

    add login form

---
+++ app/resources/views/login.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/login.css">
+
+    <title>Orange Juice Evolution!</title>
+</head>
+<body>
+    <div class="main-login">
+        <div class="left-login">
+            <h1> A Nossa comunidade <br>ainda mais vitaminada!</h1>
+            <img src="/img/orangejuice.png" class="left-login-img" alt="Orange Juice">
+        </div>
+        <div class="right-login">
+            <div class="card-login">
+                <h1>LOGIN</h1>
+                <div class="textfield">
+                    <label for="usuario">Usuário</label>
+                    <input type= "text" name="usuario" placeholder="Usuário">
+                    <div class="textfield">
+                        <label for="senha">Senha</label>
+                        <input type="password" name="senha" placeholder="Senha">
+                    </div>
+                    <button class="btn-login">Login</button>
+                    <a href="#" class="btn btn-link">
+                        Esqueceu sua senha?
+                    </a>
+            </div>
+        </div>
+    </div>
+    
+</body>
+</html>
