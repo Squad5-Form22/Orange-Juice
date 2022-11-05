dcf2323a0c1bc2585b3109d1f3f517691165ac28
savio-2-lopes <savioaugulopes@gmail.com>
2022-11-05 12:02:51 -0300

    Update layouts

--- app/resources/views/admin/dashboard.blade.php
+++ app/resources/views/admin/dashboard.blade.php
@@ -1,11 +1,11 @@
-<h1>Admin: {{$user->name}}</h1>
+<h1>Admin: {{ $user->name }}</h1>
 
 <a class="dropdown-item" href="{{ route('logout') }}"
-onclick="event.preventDefault();
-      document.getElementById('logout-form').submit();">
-{{ __('Logout') }}
+    onclick="event.preventDefault();
+              document.getElementById('logout-form').submit();">
+    {{ __('Logout') }}
 </a>
 
 <form id="logout-form" action="{{ route('logout') }}" method="post" class="d-none">
-@csrf
+    @csrf
 </form>
