5f614e9bfd5360f652031f5afc7aa90c2d3119b0
DESKTOP-R8SHGOM\home <thazsobral@gmail.com>
2022-11-05 18:19:53 -0300

    adding relationships in models

--- app/app/Models/Registration.php
+++ app/app/Models/Registration.php
@@ -8,4 +8,24 @@ use Illuminate\Database\Eloquent\Model;
 class Registration extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'trail_id',
+        'user_id',
+        'trail_finished',
+    ];
+
+    public function trails() {
+        return $this->hasOne(Trail::class, 'trail_id', 'id');
+    }
+
+    public function users() {
+        return $this->hasOne(User::class, 'user_id', 'id');
+    }
+
 }
--- app/app/Models/UserCompletedContents.php
+++ app/app/Models/UserCompletedContents.php
@@ -8,4 +8,23 @@ use Illuminate\Database\Eloquent\Model;
 class UserCompletedContents extends Model
 {
     use HasFactory;
+
+       /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'content_id',
+        'user_id',
+    ];
+
+    public function contents() {
+        return $this->hasOne(Content::class, 'content_id', 'id');
+    }
+
+    public function users() {
+        return $this->hasOne(User::class, 'user_id', 'id');
+    }
+
 }
