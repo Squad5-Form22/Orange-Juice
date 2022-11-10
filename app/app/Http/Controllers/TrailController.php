c897e2afbbf3693bbd8ddf755d90778fea5c0d66
thazsobral <thazsobral@gmail.com>
2022-11-10 03:26:37 -0300

    adjusted return values

--- app/app/Http/Controllers/TrailController.php
+++ app/app/Http/Controllers/TrailController.php
@@ -12,8 +12,8 @@ use Illuminate\Support\Facades\Auth;
 class TrailController extends Controller
 {
     public function index($trail_id) {
-        $topics = Trail::with('topics')->findOrFail($trail_id);
+        $trail = Trail::with('topics')->findOrFail($trail_id);
         
-        return view('trail/'.$trail_id)->with(['user'=>Auth::user(), 'topics'=>$topics]);
+        return ['user'=>Auth::user(), 'trail'=>$trail];
     }
 }
