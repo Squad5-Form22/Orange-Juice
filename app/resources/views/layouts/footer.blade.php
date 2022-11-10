ed79e8c4c8f6251f71dfa9cc24448a03a93fdc71
Marcus Mazza <44247641+m-mazza@users.noreply.github.com>
2022-11-10 17:47:23 -0300

    subindo landing-page sem responsividade

--- app/resources/views/layouts/footer.blade.php
+++ app/resources/views/layouts/footer.blade.php
@@ -1,7 +1,57 @@
-<footer class="bg-dark py-5">
+<footer class="footer-wrapper">
     <div class="container">
         <div class="row no-gutters justify-content-center">
-            <h1 class="mb-0 text-white">footer</h1>
+            <div class="col-12 d-flex">
+     
+                <div class="mx-auto d-flex">
+                    <div class="m-3 text-center">
+                        <img class="logo-footer"src="assets/images/institucional/logotipo/logo-orange-juice.svg">
+                        <p class="mb-0 text-white">Fique por dentro das últimas</p>
+                        <p class="mb-0 text-white">tendências tecnológicas.</p>
+                        <p class="mb-0 text-white">Confira nossos canais!</p>
+                    </div>
+                    <div class="m-3">
+                        <ul class="chanels-wrapper">
+                            <li>
+                                <a href="" class="d-flex align-items-center">
+                                    <img src="assets/images/institucional/icones/icone_spotify.svg" class="icon">
+                                    <p class="text-white mb-0">Podcast <strong>Juice Cast</strong></p>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="" class="d-flex align-items-center">
+                                    <img src="assets/images/institucional/icones/icone_twitch.svg" class="icon">
+                                    <p class="text-white mb-0">Bate-papo <strong>Orange Talks</strong></p>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="" class="d-flex align-items-center">
+                                    <img src="assets/images/institucional/icones/icone_discord.svg" class="icon">
+                                    <p class="text-white mb-0">Comunidade <strong>Orange Juice</strong></p>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="" class="d-flex align-items-center">
+                                    <img src="assets/images/institucional/icones/icone_discord.svg" class="icon">
+                                    <p class="text-white mb-0">Canal <strong>Orange</strong></p>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="" class="d-flex align-items-center">
+                                    <img src="assets/images/institucional/icones/icone_medium.svg" class="icon">
+                                    <p class="text-white mb-0"><strong>Orange</strong> Medium</p>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12">
+                <div class="mt-5">
+                    <p class="mb-0 text-white text-center">© <script>document.write(new Date().getFullYear());</script> FCamara Formação e Consultoria.</p>
+                    <p class="mb-0 text-white text-center">Todos os direitos reservados.</p>
+                </div>
+            </div>
         </div>
     </div>
 </footer>
