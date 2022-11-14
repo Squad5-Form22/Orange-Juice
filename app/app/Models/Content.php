b4c3ad0c0031a6f69050c309505297732e03517f
thazsobral <thazsobral@gmail.com>
2022-11-14 10:57:09 -0300

    add crud content

--- app/app/Models/Content.php
+++ app/app/Models/Content.php
@@ -29,4 +29,30 @@ class Content extends Model
     public function topics() {
         return $this->hasMany(Topic::class, 'topic_id');
     }
+
+    static function getAll(){
+        $data = Content::get();
+        return $data;
+    }
+
+    static function getOne($id){
+        $data = Content::where('id',$id)->get();
+        return $data;
+    }
+
+    static function saveOne($data){
+        $result = Content::create($data);
+        return $result;
+    }
+
+    static function updateOne($data, $id = null) {
+        $content = Content::findOrFail($id);        
+        $result = $content->update($data);
+        return $result;
+    }
+
+    static function deleteOne($id){
+        $result = Content::where('id',$id)->delete();
+        return $result;
+    }
 }
